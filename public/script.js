751166ae43de6dd391e2c10e9083f2e7508b1454
381706-2GuschinAlexander <43115379+381706-2GuschinAlexander@users.noreply.github.com>
2019-12-20 00:18:37 +0300

    correction to field + pre fix for id

--- public/script.js
+++ public/script.js
@@ -5,12 +5,12 @@ let player_number = 0;
 
 let ship_segments = 0;
 
-let n = 4, m = 4;
-let field1 = [[],[]];
+let n = 10, m = 10;
+let field = [[],[]];
 for (let i = 0; i < m; i++){
-    field1[i] = [];
+    field[i] = [];
     for (let j = 0; j < n; j++){
-        field1[i][j] = 0;
+        field[i][j] = 0;
     }
 }
 
@@ -28,12 +28,12 @@ function CreateTable(number){
 
   for(let i = 0; i  < 10; ++i){
     let y = document.createElement("TR");
-    y.setAttribute("id", "myTr" + i + number * 10);
+    y.setAttribute("id", "tbr" + i + number * 10);
     document.getElementById("Table" + number).appendChild(y);
     for(let k = 0; k < 10; ++k){
         let z = document.createElement("TD");
         z.innerHTML = '0';
-        document.getElementById("myTr" + i + number * 10).appendChild(z);
+        document.getElementById("tbr" + i + number * 10).appendChild(z);
       }
     }
 
@@ -44,16 +44,28 @@ function CreateTable(number){
         let td = event.target.closest('td');
         if (!td)
             return;
-        //let curr_id = td.getAttribute("id");
+        let curr_id = td.getAttribute("id");
         if(td.innerHTML != 'x'){
           ship_segments++;       
           td.innerHTML = 'x';
         }
+        let x = curr_id[3];
+        let y = curr_if[4];
+        console.log(x + " " + y + " ");
+        field[x][y] = 1;
       } 
     } else {
       x.onclick = function(event){
         if(is_started == 0)
           return;
+        let td = event.target.closest('td');
+        if (!td)
+          return;
+        let curr_id = td.getAttribute("id");
+        let x = curr_id[4];
+        let y = curr_id[5];
+
+        let xhttp = XMLHttpRequest();
       }
     }
 }
