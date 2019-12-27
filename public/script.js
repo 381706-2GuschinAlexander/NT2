c8b076e8120263cb16f730e7570987a04bb0d028
381706-2GuschinAlexander <43115379+381706-2GuschinAlexander@users.noreply.github.com>
2019-12-27 17:09:14 +0300

    final version

--- public/script.js
+++ public/script.js
@@ -1,23 +1,14 @@
 
-let is_started = 0;
+let lock = 0;
 
 let player_number = 0;
 
 let ship_segments = 0;
 
-let n = 10, m = 10;
-let field = [[],[]];
-for (let i = 0; i < m; i++){
-    field[i] = [];
-    for (let j = 0; j < n; j++){
-        field[i][j] = 0;
-    }
-}
 
-
-function getNum(){
-  player_number = 1;
-}
+let cord_inc = [];
+for(let i = 0; i < 100; ++i)
+  cord_inc.push(0);
 
 function CreateTable(number){
     let x = document.createElement("TABLE");
@@ -28,18 +19,19 @@ function CreateTable(number){
 
   for(let i = 0; i  < 10; ++i){
     let y = document.createElement("TR");
-    y.setAttribute("id", "tbr" + i + number * 10);
+    y.setAttribute("id", "tr" + (i + number * 10));
     document.getElementById("Table" + number).appendChild(y);
     for(let k = 0; k < 10; ++k){
         let z = document.createElement("TD");
         z.innerHTML = '0';
-        document.getElementById("tbr" + i + number * 10).appendChild(z);
+        z.setAttribute("id", "td" + number + i + k)
+        document.getElementById("tr" + (i + number * 10)).appendChild(z);
       }
     }
 
     if(number == 0) {
       x.onclick = function(event) {
-        if(is_started == 1)
+        if(lock == 1)
           return;
         let td = event.target.closest('td');
         if (!td)
@@ -50,22 +42,26 @@ function CreateTable(number){
           td.innerHTML = 'x';
         }
         let x = curr_id[3];
-        let y = curr_if[4];
+        let y = curr_id[4];
         console.log(x + " " + y + " ");
-        field[x][y] = 1;
+        let ind = parseInt(x * 10) + parseInt(y);
+        console.log(ind);
+        cord_inc[ind] = 2;
       } 
     } else {
       x.onclick = function(event){
-        if(is_started == 0)
+        if(lock == 0)
           return;
         let td = event.target.closest('td');
         if (!td)
           return;
         let curr_id = td.getAttribute("id");
-        let x = curr_id[4];
-        let y = curr_id[5];
-
-        let xhttp = XMLHttpRequest();
+        let x = curr_id[3];
+        let y = curr_id[4];
+        console.log(x + " " + y + " ");
+        let ind = parseInt(x * 10) + parseInt(y);
+        console.log(ind);
+        Fire(x, y);
       }
     }
 }
@@ -76,4 +72,99 @@ function CreateField(){
     CreateTable(0);
 }
 
+
+
+function Start(){
+  lock = 1;
+  document.getElementById("btn_rd").hidden = true;
+  let xhttp = new XMLHttpRequest();
+  xhttp.open("POST", "http://127.0.0.1:3000/start_the_game", true);
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      player_number = parseInt(this.response);
+      alert("You are " + player_number + " player");
+    }
+  }
+  xhttp.setRequestHeader("Content-Type", "application/json"); 
+  console.log(JSON.stringify(cord_inc));
+  xhttp.send(JSON.stringify(cord_inc));
+
+}
+
+function RQuit(){
+  if(lock != 1)
+    return;
+
+  let xhttp = new XMLHttpRequest();
+  xhttp.open("POST", "http://127.0.0.1:3000/rquit", true);
+  xhttp.send();
+}
+
+let packet = {
+  x: -1,
+  y: -1,
+  move: 1,
+  winc: 0,
+  confirm: 0
+};
+
+function Fire(x_i, y_i){
+  let xhttp = new XMLHttpRequest();
+  let recv;
+  xhttp.open("POST", "http://127.0.0.1:3000/fire", true);
+  xhttp.setRequestHeader("Content-Type", "application/json"); 
+  xhttp.responseType = 'json';
+  packet.x = x_i;
+  packet.y = y_i;
+  packet.move = player_number;
+  xhttp.send(JSON.stringify(packet));
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      console.log("reciving...");
+      recv = this.response;
+      console.log(recv);
+      if(recv.confirm == 0)
+        document.getElementById("td1" + x_i + y_i).innerHTML = "^";
+      if(recv.confirm == 1)
+        document.getElementById("td1" + x_i + y_i).innerHTML = "*";
+      if(recv.winc == 1 && recv.confirm != 1)
+        alert("2nd place");
+      else if(recv.winc == 1)
+        alert("Сongratulations! You did it!");
+
+    }
+  }
+  //UpdateT();
+}
+
+// function UpdateT(){
+//   if(lock != 1)
+//     return;
+//   else{
+//     let xhttp = new XMLHttpRequest();
+//     xhttp.open("POST", "http://127.0.0.1:3000/get_update", true);
+//     xhttp.onreadystatechange = function() {
+//      if (this.readyState == 4 && this.status == 200) {
+//         console.log("update...")
+//         let arr = this.response;
+//         if(arr[0] == 7){
+//          clearTimeout(timerId);
+//           alert("2nd place!");
+//         } 
+//         for(let it = 0; it < 100; it++){
+//           if(arr[it] == 0)
+//             document.getElementById("td");
+//       }
+//     }
+//   } 
+//   xhttp.setRequestHeader("Content-Type", "application/json"); 
+//   xhttp.responseType = 'json';
+//   console.log((JSON.stringify(player_number)));
+//   xhttp.send((JSON.stringify(player_number)));
+// }
+// }
+
+// let upd = UpdateT();
+
+// let timerId = setInterval(() => UpdateT(), 5000);
 
--- server.js
+++ server.js
@@ -2,15 +2,28 @@ let express = require("express");
 let app = express();
 let bodyParser = require("body-parser");
 
+let fir_field;
+let sec_field;
+
+let first_count = 0;
+let second_count = 0;
+let turn = 0;
+
+let date_start;
+let start_min;
+let end_min;
+let winner = 0;
 let player_count = 0;
 let ready_pl = 0;
 let urlencodedParser = bodyParser.urlencoded({extended: true});
 
 app.use(express.static("public"));
+app.use(express.urlencoded({extended: true}));
+app.use(bodyParser.json());
 
 app.get("/index.html",function(request, response){
-    player_count++;
-    console.log(player_count + "count");
+    //player_count++;
+    console.log(player_count + " count");
     response.sendFile(__dirname + "/index.html");
 });
 
@@ -18,21 +31,155 @@ app.post("/index.html", urlencodedParser, function(req, res) {
     return res.send(String(player_count));
 });
 
-app.get("/game.html",function(request, response){
-    let a = request
+app.post("/game_count", urlencodedParser, function(req, res) {
+    console.log("cliked");
+    player_count++;
 });
 
+app.post("/start_the_game", urlencodedParser, function(req, res) {
+    ready_pl++;
+    console.log(ready_pl + " ready ");
+    if(ready_pl == 1){
+        fir_field = req.body;
+        console.log(fir_field);
+        for(let it = 0; it < 100; it++)
+            if(fir_field[it] == 2) first_count++;
 
-// app.get("/",function(request, response){
-    
-// });
+        console.log("first count: " + first_count);
+    }
+    if(ready_pl == 2){
+        date_start = new Date().toISOString().slice(0, 19).replace('T', ' ');
+        console.log(date_start);
+        start_min = new Date().getMinutes();
+        console.log( "start min: " + start_min);
+        turn = 1;
+        sec_field = req.body;
+        console.log(sec_field);
+        for(let it = 0; it < 100; it++)
+            if(sec_field[it] == 2) second_count++;
+                
+        console.log("second count: " + second_count);
+    }
+    return res.send(String(ready_pl));
+});
+
+let tmp = {
+    x: -1,
+    y: -1,
+    move: 1,
+    winc: 0,
+    confirm: 0
+  };
+
+app.post("/rquit", urlencodedParser, function(req, res) {
+    ready_pl = 0;
+    turn = 0;
+    first_count = 0;
+    second_count = 0;
+    player_count = 0;
+    tmp.winc = 0;
+    tmp.move = 1;
+    winner = 0;
+    console.log("restart");
+});
+
+app.post("/fire", urlencodedParser, function(req, res) {
+    pr = req.body;
+    console.log(JSON.stringify(pr));
+    if(turn != pr.move){
+        tmp.confirm = -1;
+        res.json(tmp);
+    } else if(turn == 1 || turn == 2){    
+        let x_i = pr.x;
+        let y_i = pr.y;
+        if(turn == 1){
+            let ind = parseInt(x_i * 10) + parseInt(y_i);
+            if(sec_field[ind] == 2){
+                console.log("1 player hit at " + x_i + y_i);
+                sec_field[ind] = 3;
+                tmp.confirm = 1;
+                second_count--;
+                if(second_count == 0){
+                    tmp.winc = 1;
+                    winner = 1;
+                    end_min = new Date().getMinutes();  
+                    let sql1 = "INSERT INTO history(player_name, date, duration)";
+                    sql1 +=   "VALUES('" + winner + " player','" + date_start + "', '" + Math.abs(parseInt(end_min) - parseInt(start_min)) + "');";
+                    console.log(sql1);
+                    con.query(sql1, function(err, results) {
+                        if (err) throw err;
+                        console.log("Insert copmlete");
+                    });
+                }
+            } else if(sec_field[ind] == 0){
+                console.log("1 player missed " + x_i + y_i);
+                sec_field[ind] = 1;
+                turn = 2;
+                tmp.confirm = 0;
+            } else {
+                console.log("smh 1");
+                tmp.confirm = -1;
+            }
+        } else if(turn == 2){
+            let ind = parseInt(x_i * 10) + parseInt(y_i);
+            if(fir_field[ind] == 2){
+                console.log("2 player hit at " + x_i + y_i);
+                fir_field[ind] = 3;
+                tmp.confirm = 1;
+                first_count--;
+                if(first_count == 0){
+                    tmp.winc = 1;
+                    winner = 2;
+                    end_min = new Date().getMinutes();  
+                    let sql1 = "INSERT INTO history(player_name, date, duration)";
+                    sql1 +=   "VALUES('" + winner + " player','" + date_start + "', '" + Math.abs(parseInt(end_min) - parseInt(start_min)) + "');";
+                    console.log(sql1);
+                    con.query(sql1, function(err, results) {
+                        if (err) throw err;
+                        console.log("Insert copmlete");
+                    });
+                }
+            } else if(fir_field[ind] == 0){
+                console.log("2 player missed " + x_i + y_i);
+                fir_field[ind] = 1;
+                turn = 1;
+                tmp.confirm = 0;
+            } else {
+                console.log("smh 1");
+                tmp.confirm = -1;
+                end_min = new Date().getMinutes();
+            }
+        }
+        res.json(tmp);
+    }
+});
+
+app.get("/database",function(request, response)
+{
+    con.query("SELECT * FROM mydb.history", function (err, result, fields) {
+        if (err) throw err;
+        console.log(result);
+        response.setHeader("UserId", 12);
+        response.setHeader("Content-Type", "text/html; charset=utf-8;");
+        console.log(JSON.stringify(result));
+        response.write(JSON.stringify(result));
+        response.end();
+      });
+});
 
 app.listen(3000);
 
-app.use(bodyParser.json());
+var mysql = require('mysql');
 
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "1234",
+  database: "mydb"
+});
 
-/*app.post('/index.html', urlencodedParser, function(req, res) {
-    
-    return res.json(request);
-});*/
+con.connect(function(err) {
+     if (err) throw err;
+     console.log("Connected!");
+  });
+
